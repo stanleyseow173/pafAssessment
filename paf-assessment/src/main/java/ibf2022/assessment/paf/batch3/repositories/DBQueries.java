3e330c9dbac773ca9203c73c755276d335b6d3c5
stanleyseow173 <seowszehowestanley@gmail.com>
2023-04-28 11:43:34 +0800

    refine task 2 and 3

--- paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/repositories/DBQueries.java
+++ paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/repositories/DBQueries.java
@@ -1,7 +1,7 @@
 package ibf2022.assessment.paf.batch3.repositories;
 
 public class DBQueries {
-    public static final String GET_STYLES = "select s.id, style_name, count(b.id) as beer_count from styles s, beers b where b.style_id = s.id group by s.id order by beer_count DESC, style_name;";
+    public static final String GET_STYLES = "select s.id, style_name, count(b.id) as beer_count from styles s left join beers b on s.id = b.style_id group by s.id order by beer_count DESC, style_name;";
 
     public static final String GET_BREWERIES = "select b.id as beer_id, b.name as beer_name, b.descript as description, w.id as brewery_id, w.name as brewery_name from beers b LEFT JOIN breweries w ON b.brewery_id = w.id where b.style_id = ? order by beer_name ASC;";
 
